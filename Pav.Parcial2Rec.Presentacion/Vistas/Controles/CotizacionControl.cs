158738a29541094423f51eea99c46cdd43482389
Daniel Aybar <91288441+daniaybarcode@users.noreply.github.com>
2024-08-08 13:40:22 -0300

    mejora componente

--- Pav.Parcial2Rec.Presentacion/Vistas/Controles/CotizacionControl.cs
+++ Pav.Parcial2Rec.Presentacion/Vistas/Controles/CotizacionControl.cs
@@ -27,36 +27,37 @@ namespace Pav.Parcial2Rec.Presentacion.Vistas
 
         private void InitializeComponent()
         {
-            this.lblFechaHora = new System.Windows.Forms.Label();
-            this.lblValor = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            lblFechaHora = new Label();
+            lblValor = new Label();
+            SuspendLayout();
             // 
             // lblFechaHora
             // 
-            this.lblFechaHora.AutoSize = true;
-            this.lblFechaHora.Location = new System.Drawing.Point(3, 9);
-            this.lblFechaHora.Name = "lblFechaHora";
-            this.lblFechaHora.Size = new System.Drawing.Size(38, 15);
-            this.lblFechaHora.TabIndex = 0;
-            this.lblFechaHora.Text = "Fecha";
+            lblFechaHora.AutoSize = true;
+            lblFechaHora.Location = new Point(17, 9);
+            lblFechaHora.Name = "lblFechaHora";
+            lblFechaHora.Size = new Size(38, 15);
+            lblFechaHora.TabIndex = 0;
+            lblFechaHora.Text = "Fecha";
             // 
             // lblValor
             // 
-            this.lblValor.AutoSize = true;
-            this.lblValor.Location = new System.Drawing.Point(200, 9);
-            this.lblValor.Name = "lblValor";
-            this.lblValor.Size = new System.Drawing.Size(33, 15);
-            this.lblValor.TabIndex = 1;
-            this.lblValor.Text = "Valor";
+            lblValor.AutoSize = true;
+            lblValor.Location = new Point(189, 9);
+            lblValor.Name = "lblValor";
+            lblValor.Size = new Size(33, 15);
+            lblValor.TabIndex = 1;
+            lblValor.Text = "Valor";
             // 
             // CotizacionControl
             // 
-            this.Controls.Add(this.lblValor);
-            this.Controls.Add(this.lblFechaHora);
-            this.Name = "CotizacionControl";
-            this.Size = new System.Drawing.Size(300, 30);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            BorderStyle = BorderStyle.FixedSingle;
+            Controls.Add(lblValor);
+            Controls.Add(lblFechaHora);
+            Name = "CotizacionControl";
+            Size = new Size(288, 33);
+            ResumeLayout(false);
+            PerformLayout();
         }
     }
 }
